b4183d90e93d1e1e9d63f56c816f4d938daf9e58
riturajchoudhury <riturajchoudhury112@gmail.com>
2026-03-28 03:04:04 +0530

    testing

---
+++ DAA/FindMAxandMin.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i;
+    int max, min;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements: ");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    max = arr[0];
+    min = arr[0];
+
+    for(i = 1; i < n; i++) {
+        if(arr[i] > max)
+            max = arr[i];
+
+        if(arr[i] < min)
+            min = arr[i];
+    }
+
+    printf("Maximum element: %d\n", max);
+    printf("Minimum element: %d", min);
+
+    return 0;
+}
